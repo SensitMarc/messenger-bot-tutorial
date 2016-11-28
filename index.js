9ee3832f05dfea85d4d7e351e2a2aa5055c5d790
SensitMarc <sensitlabs@gmail.com>
2016-11-27 22:36:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function sendTextMessage(sender, text) {
 	
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
+		qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN},
 		method: 'POST',
 		json: {
 			recipient: {id:sender},
