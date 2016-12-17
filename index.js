cf672cae051b573fe2108e49828e0f55a5e4007f
SensitMarc <sensitlabs@gmail.com>
2016-12-17 19:57:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -62,6 +62,50 @@ app.post('/webhook/', function (req, res) {
  const token = process.env.FB_PAGE_ACCESS_TOKEN_SENSEE
 //const token = "FB_PAGE_ACCESS_TOKEN"
 
+  "setting_type" : "call_to_actions",
+  "thread_state" : "existing_thread",
+  "call_to_actions":[
+    {
+      "type":"postback",
+      "title":"Help",
+      "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_HELP"
+    },
+    {
+      "type":"postback",
+      "title":"Start a New Order",
+      "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_START_ORDER"
+    },
+    {
+      "type":"web_url",
+      "title":"Checkout",
+      "url":"http://www.sensee.ca",
+      "webview_height_ratio": "full",
+      "messenger_extensions": true
+    },
+    {
+      "type":"web_url",
+      "title":"View Website",
+      "url":"http://www.sensee.ca"
+    }
+  ]
+}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN_SENSEE},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
 function sendTextMessage(sender, text) {
 let	messageData = { text:text }
 	
@@ -222,55 +266,8 @@ function sendGenericMessage(sender) {
 		}
 	})
 }
-/*
-function processLocation(sender, coords) {
-    httpRequest({
-        url: 'https://api.keen.io/3.0/projects/563a13c896773d4a75c3bf93/queries/count_unique?api_key=9d45d36b3d3040533eb3a9f2e8bcc9e317d6b4e7e2cbb413ce959e7c0f8b926a7b82523fc0acd774ef024a0f4bddcc2bd8e992e2f61d9aed7b7f09bcb63bc0a1ebee2e0ebd0e2792dba3dc4ae1ae9c11c19e54753574a726dea4eec16e463aa06196d6876d167a9d668f236f25a8857d&event_collection=kWhr&target_property=kWhr&timezone=UTC&timeframe=this_1_days&filters=%5B%5D',
-        method: 'GET'
-    }, function(error, response, body) {
-        var station,
-            messageData,
-            directionsUrl;
-        if (! error && response.statusCode === 200) {
-            station = JSON.parse(body);
-            directionsUrl = 'http://bing.com/maps/default.aspx?rtop=0~~&rtp=pos.' + coords.lat + '_' + coords.long + '~pos.' + station.gtfs_latitude + '_' + station.gtfs_longitude + '&mode=';
-            // Walkable if 2 miles or under
-            directionsUrl += (station.distance <= 2 ? 'W' : 'D');
-            messageData = {
-                'attachment': {
-                    'type': 'template',
-                    'payload': {
-                        'template_type': 'generic',
-                        'elements': [{
-                            'title': 'Closest BART: ' + station.name,
-                            'subtitle': station.distance.toFixed(2) + ' miles',
-                            'image_url': 'https://api.mapbox.com/v4/mapbox.streets/' + station.gtfs_longitude + ',' + station.gtfs_latitude + ',18/640x480@2x.png?access_token=' + MAPBOX_API_TOKEN,
-                            'buttons': [{
-                                'type': 'web_url',
-                                'url': 'http://www.bart.gov/stations/' + station.abbr.toLowerCase(),
-                                'title': 'Station Information'
-                            }, {
-                                'type': 'postback',
-                                'title': 'Departures',
-                                'payload': 'departures ' + station.abbr,
-                            }, {
-                                'type': 'web_url',
-                                'url': directionsUrl,
-                                'title': 'Directions'
-                            }]
-                        }]
-                    }
-                }
-            };
-            sendGenericMessage(sender, messageData);
-        } else {
-            console.log(error);
-            sendTextMessage(sender, 'Sorry I was unable to determine your closest BART station.');
-        }
-    });   
-}
-*/
-	
+
+
 // spin spin sugar
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
