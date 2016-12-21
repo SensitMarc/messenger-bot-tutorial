7ac9aae533ae02f73d9dc2e1cd8ccc399bd7b2b6
SensitMarc <sensitlabs@gmail.com>
2016-12-20 22:42:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -65,11 +65,12 @@ app.post('/webhook/', function (req, res) {
 
 // recommended to inject access tokens as environmental variables, e.g.
  const token = process.env.FB_PAGE_ACCESS_TOKEN_SENSEE
+ const token = process.env.DEVICE_ACCESS_TOKEN
 //const token = "FB_PAGE_ACCESS_TOKEN"
 
- function start() {
+/* function start() {
 
-        var eventSource = new EventSource("https://api.spark.io/v1/devices/" + deviceID + "/events/?access_token=" + accessToken);
+        var eventSource = new EventSource("  ");
 
         eventSource.addEventListener('open', function(e) {
             console.log("Opened!"); },false);
@@ -79,15 +80,9 @@ app.post('/webhook/', function (req, res) {
 
         eventSource.addEventListener('Uptime', function(e) {
             var parsedData = JSON.parse(e.data);
-            var tempSpan = document.getElementById("uptime");
-            var tsSpan   = document.getElementById("tstamp");
-            tempSpan.innerHTML = "Core: " + parsedData.coreid + " uptime: " + parsedData.data + " (h:m:s)";
-            tempSpan.style.fontSize = "28px";
-            tsSpan.innerHTML = "At timestamp " + parsedData.published_at;
-            tsSpan.style.fontSize = "9px";
         }, false);
     }
-
+    */
 
  function addPersistentMenu(){
  request({
@@ -152,7 +147,8 @@ let	messageData = { text:text }
 function getReal(sender){
 httpRequest({
     //    url:'https://api.keen.io/3.0/projects/563a13c896773d4a75c3bf93/queries/count_unique?api_key=9d45d36b3d3040533eb3a9f2e8bcc9e317d6b4e7e2cbb413ce959e7c0f8b926a7b82523fc0acd774ef024a0f4bddcc2bd8e992e2f61d9aed7b7f09bcb63bc0a1ebee2e0ebd0e2792dba3dc4ae1ae9c11c19e54753574a726dea4eec16e463aa06196d6876d167a9d668f236f25a8857d&event_collection=kWhr&target_property=kWhr&timezone=UTC&timeframe=this_1_days&filters=%5B%5D',
-    	url:'https://api.particle.io/v1/devices/48ff6e065067555013541287/watts?access_token=83f31cc92f11fd6c6b2c7fe2eb37b7b73b8584d1',
+    	url:'https://api.particle.io/v1/devices/48ff6e065067555013541287/watts',
+	qs: {access_token:process.env.DEVICE_ACCESS_TOKEN},
 	method: 'GET'
     }, function(error, response, body) {
         var maya;
