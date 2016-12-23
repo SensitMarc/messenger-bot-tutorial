97b369778b45db62ad4a37ebb089fbb10131f9f3
SensitMarc <sensitlabs@gmail.com>
2016-12-22 23:18:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.post('/webhook/', function (req, res) {
             {
               type:'postback',
               title:'Status',
-              payload: 'getReal(sender)'
+              payload: 'status'
             },
             {
               type:'postback',
