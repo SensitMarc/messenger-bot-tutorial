70521a08ea4543a28f0b3b89454b15d8fb1868f9
SensitMarc <sensitlabs@gmail.com>
2016-12-23 09:34:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/webhook/', function (req, res) {
             {
               type:'postback',
               title:'Status',
-              payload:  webView(sender)
+              payload:  {'USER_DEFINED_PAYLOAD',  'title' : "Find a product"}
             },
             {
               type:'postback',
