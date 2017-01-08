8001e30d048b2ca355d99963801042e2ea7f2ccb
SensitMarc <sensitlabs@gmail.com>
2017-01-08 09:52:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post('/webhook/', function (req, res) {
 		      continue
 	      }
 		if (text === 'power') {
-		getReal(sender)
+		getReal(sender),
 		getTemperature(sender)
 		     continue 
 	      }      
