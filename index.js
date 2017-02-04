93bfc2e3186aa69057bcc977c8c1e99cf0c97dc5
SensitMarc <sensitlabs@gmail.com>
2017-02-04 17:29:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ app.get('/webhook/', function (req, res) {
 	}
 	res.send('Error, wrong token')
 })
+//var sslmode=require('sslmode');
+
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 /*
 
 app.post('/webhook/', function (req, res) {
@@ -417,22 +432,6 @@ function sendGenericMessage(sender) {
 		}
 	})
 }
-
-//var sslmode=require('sslmode');
-
-var pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
 
 
 // spin spin sugar
