a4a927a594d450c6c91af18d3b909c604ab7c5ae
SensitMarc <sensitlabs@gmail.com>
2017-02-13 18:36:14 -0400

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -20,6 +20,59 @@ app.get('/', function (req, res) {
 	res.send('hello world i am a secret booty')
 })
 
+function sendGenericMessage() {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "First tip",
+					"subtitle": "check this out",
+					"image_url": "https://raw.githubusercontent.com/SensitMarc/dashboards/gh-pages/house-128.png",
+					"buttons": [{
+						 "type": "web_url",
+                        			"url": "http://sensee.ca/prototypes/index.html",
+                       				 "title": "my house",
+						 "webview_height_ratio": "compact"
+					}, {
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for first element in a generic bubble",
+					}],
+				}, {
+					"title": "Second card",
+					"subtitle": "Element #2 of an hscroll",
+					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.pn",
+					"buttons": [{
+						"type": "postback",
+						"title": "Postback",
+						"payload": "Payload for second element in a generic bubble",
+					}],
+				}]
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN_SENSEE},
+		method: 'POST',
+		json: {
+			recipient: {id:process.env.sender_id},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+sendGenericMessage();
+
+/*
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN_SENSEE},
@@ -36,3 +89,4 @@ app.get('/', function (req, res) {
 		}
 	})
 
+*/
