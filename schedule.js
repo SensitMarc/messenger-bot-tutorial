ca2bf1458fcff49fd21994f4cb0f67b180a43169
SensitMarc <sensitlabs@gmail.com>
2017-02-13 17:35:23 -0400

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,1 +1,18 @@
 console.log("Maya");
+
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:process.env.FB_PAGE_ACCESS_TOKEN_SENSEE},
+		method: 'POST',
+		json: {
+			recipient: {id:sender_id},
+			message: ("text": "Space"},
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+
