2357e9104393dd430e9015f386659ae4c01a01c0
SensitMarc <sensitlabs@gmail.com>
2017-03-15 19:47:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ function addPersistentMenu(){
   persistent_menu:[
     {
       "locale":"default",
-      "composer_input_disabled":true,
+      "composer_input_disabled":false,
           "call_to_actions":[
             {
               "title":"MAIN MENU",
