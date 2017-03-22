cbe469889a5ee2e80dca66e1b00186886bbaa2f1
SensitMarc <sensitlabs@gmail.com>
2017-03-22 20:55:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -79,13 +79,14 @@ app.post('/webhook/', function (req, res) {
 		      	      
         sendTextMessage(sender, text.substring(0, 200))
       }
-   /*
+   
 	    if (event.postback && event.postback.payload) {
         text = JSON.stringify(event.postback.payload)
-	      if (text === power){
+	      if (text === MAIN MENU){
+		      webView(sender)
         //sendTextMessage(sender, "Postback received: "+ text.substring(0, 200))
-	getReal(sender, JSON.stringify(event.postback.payload))
-	console.log(	      
+	//getReal(sender, JSON.stringify(event.postback.payload))
+	      
 	continue
 	      }
       }
